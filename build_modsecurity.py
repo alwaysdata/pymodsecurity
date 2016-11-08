695fe8b06fca2f34572763ffd72d6db8197e5e6f
David Testé <soonum@gnu.org>
2016-11-08 12:07:06 +0100

    Remove static filepath, move and rename .h files

--- build_modsecurity.py
+++ build_modsecurity.py
@@ -2,24 +2,35 @@
 # coding: utf-8
 
 import cffi
+import os
 
-# Static path to the libraries.
-# Has to be changed to be user independant.
-path_to_libraries = ["/home/soonum/Code/alwaysdata/ModSecurity/examples/cffi_tests/modsecurity/lib/"]
+
+source_file = "modsecurity_transaction_rules_source.h"
+cdef_file = "modsecurity_transaction_rules_cdef.h"
 libraries = ["modsecurity"]
-path = "/home/soonum/Code/alwaysdata/ModSecurity/examples/cffi_tests/modsecurity_includes_clean/"
-path_to_headers = path + "modsecurity_transaction_rules_headers.h"
-path_to_source = path + "modsecurity_transaction_rules_cleaned.h"
+working_directory = os.path.abspath(os.path.dirname(__file__))
+
+path_to_libraries = working_directory + "/lib/"
+if not os.path.isdir(path_to_libraries):
+    message = path_to_libraries + " doesn't exist"
+    raise FileNotFoundError(message)
+
+path_to_source = working_directory + "/build_src/" + source_file
+path_to_cdef = working_directory + "/build_src/" + cdef_file
+if not os.path.isfile(path_to_source) or not os.path.isfile(path_to_cdef):
+    message = source_file + " or " + cdef_file + " don't exist"
+    raise FileNotFoundError(message)
+
 
 ffibuilder = cffi.FFI()
 
-with open(path_to_headers, 'r') as f:
+with open(path_to_source, 'r') as f:
     ffibuilder.set_source("_modsecurity",
                           f.read(),
-                          library_dirs=path_to_libraries,
+                          library_dirs=[path_to_libraries],
                           libraries=libraries)
 
-with open(path_to_source, 'r') as f:
+with open(path_to_cdef, 'r') as f:
     ffibuilder.cdef(f.read())
 
 if __name__ == "__main__":
