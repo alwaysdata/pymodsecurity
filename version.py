03d92f0b7de8f5b0082ac5734ed0c4b0fbe21802
David Testé <soonum@gnu.org>
2016-11-23 17:42:39 +0100

    Add version file containing metadata

---
+++ version.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+pyModSecurity - Python bindings for ModSecurity library
+"""
+
+__all__ = ["__author__",
+           "__copyright__",
+           "__email__",
+           "__license__",
+           "__summary__",
+           "__title__",
+           "__uri__",
+           "__version__"]
+
+__version__ = "1.0.0.b1"
+
+__title__ = "pyModSecurity"
+__uri__ = "https://github.com/SpiderLabs/ModSecurity-Python-bindings"
+__summary__ = "Python bindings for ModSecurity library"
+__author__ = "David Teste"
+__email__ = "soonum@gnu.org"
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright 2016 {0}".format(__author__)
