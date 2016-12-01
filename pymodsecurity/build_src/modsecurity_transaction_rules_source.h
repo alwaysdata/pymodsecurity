e6c1a6274a57e036888d1e7ef47830d030d34d68
David Testé <soonum@gnu.org>
2016-12-01 11:37:54 +0100

    Clean file

--- pymodsecurity/build_src/modsecurity_transaction_rules_source.h
+++ pymodsecurity/build_src/modsecurity_transaction_rules_source.h
@@ -1,4 +1,13 @@
 /*
+ * Source file used for building CFFI module
+ *
+ * Building references :
+ *     - libmodsecurity version : v3.0.0
+ *     - commit hash : ab88083
+ */
+
+
+/*
  * modsecurity.h section
  */
 
@@ -36,15 +45,10 @@ typedef struct ModSecurity_t ModSecurity;
 
 typedef void (*LogCb) (void *, const char *);
 
-/** @ingroup ModSecurity_C_API */
 ModSecurity *msc_init(void);
-/** @ingroup ModSecurity_C_API */
 const char *msc_who_am_i(ModSecurity *msc);
-/** @ingroup ModSecurity_C_API */
 void msc_set_connector_info(ModSecurity *msc, const char *connector);
-/** @ingroup ModSecurity_C_API */
 void msc_set_log_cb(ModSecurity *msc, LogCb cb);
-/** @ingroup ModSecurity_C_API */
 void msc_cleanup(ModSecurity *msc);
 
 /*
@@ -85,59 +89,43 @@ typedef struct Rules_t Rules;
 Transaction *msc_new_transaction(ModSecurity *ms,
     Rules *rules, void *logCbData);
 
-/** @ingroup ModSecurity_C_API */
 int msc_process_connection(Transaction *transaction,
     const char *client, int cPort, const char *server, int sPort);
 
-/** @ingroup ModSecurity_C_API */
 int msc_process_request_headers(Transaction *transaction);
 
-/** @ingroup ModSecurity_C_API */
 int msc_add_request_header(Transaction *transaction, const unsigned char *key,
     const unsigned char *value);
 
-/** @ingroup ModSecurity_C_API */
 int msc_process_request_body(Transaction *transaction);
 
-/** @ingroup ModSecurity_C_API */
 int msc_append_request_body(Transaction *transaction,
     const unsigned char *body, size_t size);
 
-/** @ingroup ModSecurity_C_API */
 int msc_request_body_from_file(Transaction *transaction, const char *path);
 
-/** @ingroup ModSecurity_C_API */
 int msc_process_response_headers(Transaction *transaction, int code,
     const char* protocol);
 
-/** @ingroup ModSecurity_C_API */
 int msc_add_response_header(Transaction *transaction,
     const unsigned char *key, const unsigned char *value);
 
-/** @ingroup ModSecurity_C_API */
 int msc_process_response_body(Transaction *transaction);
 
-/** @ingroup ModSecurity_C_API */
 int msc_append_response_body(Transaction *transaction,
     const unsigned char *body, size_t size);
 
-/** @ingroup ModSecurity_C_API */
 int msc_process_uri(Transaction *transaction, const char *uri,
     const char *protocol, const char *http_version);
 
-/** @ingroup ModSecurity_C_API */
 const char *msc_get_response_body(Transaction *transaction);
 
-/** @ingroup ModSecurity_C_API */
 int msc_get_response_body_length(Transaction *transaction);
 
-/** @ingroup ModSecurity_C_API */
 void msc_transaction_cleanup(Transaction *transaction);
 
-/** @ingroup ModSecurity_C_API */
 int msc_intervention(Transaction *transaction, ModSecurityIntervention *it);
 
-/** @ingroup ModSecurity_C_API */
 int msc_process_logging(Transaction *transaction);
 
 /*
