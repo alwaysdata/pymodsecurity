86c4e4ae45bcbb59f076f09f8a9766c694ebff70
David Testé <soonum@gnu.org>
2016-12-05 17:13:56 +0100

    Remove blank lines and indent code

--- pymodsecurity/build_src/modsecurity_transaction_rules_cdef.h
+++ pymodsecurity/build_src/modsecurity_transaction_rules_cdef.h
@@ -1,9 +1,10 @@
 /*
  * Cdef file used for building CFFI module
+ * This file must NOT containe `#include` directives.
  *
  * Building references :
  *     - libmodsecurity version : v3.0.0
- *     - commit hash : ab88083
+ *     - commit hash : 47f2e7f
  */
 
 
@@ -37,45 +38,40 @@ typedef struct Rules_t Rules;
 
 
 Transaction *msc_new_transaction(ModSecurity *ms,
-    Rules *rules, void *logCbData);
-
+				 Rules *rules,
+				 void *logCbData);
 int msc_process_connection(Transaction *transaction,
-    const char *client, int cPort, const char *server, int sPort);
-
+			   const char *client,
+			   int cPort,
+			   const char *server,
+			   int sPort);
 int msc_process_request_headers(Transaction *transaction);
-
-int msc_add_request_header(Transaction *transaction, const unsigned char *key,
-    const unsigned char *value);
-
+int msc_add_request_header(Transaction *transaction,
+			   const unsigned char *key,
+			   const unsigned char *value);
 int msc_process_request_body(Transaction *transaction);
-
 int msc_append_request_body(Transaction *transaction,
-    const unsigned char *body, size_t size);
-
+			    const unsigned char *body,
+			    size_t size);
 int msc_request_body_from_file(Transaction *transaction, const char *path);
-
-int msc_process_response_headers(Transaction *transaction, int code,
-    const char* protocol);
-
+int msc_process_response_headers(Transaction *transaction,
+				 int code,
+				 const char* protocol);
 int msc_add_response_header(Transaction *transaction,
-    const unsigned char *key, const unsigned char *value);
-
+			    const unsigned char *key,
+			    const unsigned char *value);
 int msc_process_response_body(Transaction *transaction);
-
 int msc_append_response_body(Transaction *transaction,
-    const unsigned char *body, size_t size);
-
-int msc_process_uri(Transaction *transaction, const char *uri,
-    const char *protocol, const char *http_version);
-
+			     const unsigned char *body, 
+			     size_t size);
+int msc_process_uri(Transaction *transaction, 
+		    const char *uri,
+		    const char *protocol,
+		    const char *http_version);
 const char *msc_get_response_body(Transaction *transaction);
-
 int msc_get_response_body_length(Transaction *transaction);
-
 void msc_transaction_cleanup(Transaction *transaction);
-
 int msc_intervention(Transaction *transaction, ModSecurityIntervention *it);
-
 int msc_process_logging(Transaction *transaction);
 
 /*
@@ -87,8 +83,10 @@ typedef struct Rules_t Rules;
 Rules *msc_create_rules_set(void);
 void msc_rules_dump(Rules *rules);
 int msc_rules_merge(Rules *rules_dst, Rules *rules_from);
-int msc_rules_add_remote(Rules *rules, const char *key, const char *uri,
-    const char **error);
+int msc_rules_add_remote(Rules *rules,
+			 const char *key, 
+			 const char *uri,
+			 const char **error);
 int msc_rules_add_file(Rules *rules, const char *file, const char **error);
 int msc_rules_add(Rules *rules, const char *plain_rules, const char **error);
 int msc_rules_cleanup(Rules *rules);
--- pymodsecurity/build_src/modsecurity_transaction_rules_source.h
+++ pymodsecurity/build_src/modsecurity_transaction_rules_source.h
@@ -3,7 +3,7 @@
  *
  * Building references :
  *     - libmodsecurity version : v3.0.0
- *     - commit hash : ab88083
+ *     - commit hash : 47f2e7f
  */
 
 
@@ -87,45 +87,40 @@ typedef struct Rules_t Rules;
     yajl_gen_integer(g, b);
 
 Transaction *msc_new_transaction(ModSecurity *ms,
-    Rules *rules, void *logCbData);
-
+				 Rules *rules,
+				 void *logCbData);
 int msc_process_connection(Transaction *transaction,
-    const char *client, int cPort, const char *server, int sPort);
-
+			   const char *client,
+			   int cPort,
+			   const char *server,
+			   int sPort);
 int msc_process_request_headers(Transaction *transaction);
-
-int msc_add_request_header(Transaction *transaction, const unsigned char *key,
-    const unsigned char *value);
-
+int msc_add_request_header(Transaction *transaction,
+			   const unsigned char *key,
+			   const unsigned char *value);
 int msc_process_request_body(Transaction *transaction);
-
 int msc_append_request_body(Transaction *transaction,
-    const unsigned char *body, size_t size);
-
+			    const unsigned char *body,
+			    size_t size);
 int msc_request_body_from_file(Transaction *transaction, const char *path);
-
-int msc_process_response_headers(Transaction *transaction, int code,
-    const char* protocol);
-
+int msc_process_response_headers(Transaction *transaction,
+				 int code,
+				 const char* protocol);
 int msc_add_response_header(Transaction *transaction,
-    const unsigned char *key, const unsigned char *value);
-
+			    const unsigned char *key,
+			    const unsigned char *value);
 int msc_process_response_body(Transaction *transaction);
-
 int msc_append_response_body(Transaction *transaction,
-    const unsigned char *body, size_t size);
-
-int msc_process_uri(Transaction *transaction, const char *uri,
-    const char *protocol, const char *http_version);
-
+			     const unsigned char *body,
+			     size_t size);
+int msc_process_uri(Transaction *transaction, 
+		    const char *uri,
+		    const char *protocol,
+		    const char *http_version);
 const char *msc_get_response_body(Transaction *transaction);
-
 int msc_get_response_body_length(Transaction *transaction);
-
 void msc_transaction_cleanup(Transaction *transaction);
-
 int msc_intervention(Transaction *transaction, ModSecurityIntervention *it);
-
 int msc_process_logging(Transaction *transaction);
 
 /*
@@ -144,8 +139,10 @@ typedef struct Rules_t Rules;
 Rules *msc_create_rules_set(void);
 void msc_rules_dump(Rules *rules);
 int msc_rules_merge(Rules *rules_dst, Rules *rules_from);
-int msc_rules_add_remote(Rules *rules, const char *key, const char *uri,
-    const char **error);
+int msc_rules_add_remote(Rules *rules,
+			 const char *key,
+			 const char *uri,
+			 const char **error);
 int msc_rules_add_file(Rules *rules, const char *file, const char **error);
 int msc_rules_add(Rules *rules, const char *plain_rules, const char **error);
 int msc_rules_cleanup(Rules *rules);
