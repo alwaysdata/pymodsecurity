25de7a185e9b592f44c592e1d8af003cb38142db
David Testé <soonum@gnu.org>
2016-12-05 17:23:18 +0100

    Add set_log_callback implementation

--- pymodsecurity/modsecurity.py
+++ pymodsecurity/modsecurity.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 pymodsecurity.modsecurity
------------------------
+--------------------------
 
 Provide a class :class:`ModSecurity` gathering methods coming from
-libmodsecurity C interface via CFFI engine.
+libmodsecurity.
 """
 
 import functools
@@ -13,35 +13,50 @@ from pymodsecurity._modsecurity import ffi as _ffi
 from pymodsecurity._modsecurity import lib as _lib
 from pymodsecurity import utils
 
-NULL = _ffi.NULL
+_NULL = _ffi.NULL
 
 
 class ModSecurity:
     """
-    Wrapper for C function built from modsecurity.h via CFFI.
+    Wrapper for C function built from **modsecurity.h** via CFFI.
     """
     def __init__(self,):
         _modsecurity_struct = _lib.msc_init()
-        assert _modsecurity_struct != NULL
+        assert _modsecurity_struct != _NULL
         self._modsecurity_struct = _ffi.gc(_modsecurity_struct,
                                            _lib.msc_cleanup)
 
-        self._id = self.who_am_i()
-        self._log_callback = NULL
+        self._log_callback = _NULL
 
-    def set_log_callback(self, modsec, callback):
+    def set_log_callback(self, callback):
         """
         Set the log callback function.
 
-        It is neccessary to indicate to libModSecurity which function within
+        It is neccessary to indicate to ModSecurity which function within
         the connector should be called when logging is required.
 
-        :note: This method is not usable yet.
+        :param callback: Python callable object
+
+        .. note:: The callback should perform few operations or even none on
+            datas.
+
+        .. warning:: Be careful when writing the Python callback function. If
+            it returns an object of the wrong type, or more generally raises
+            an exception, the exception cannot be propagated. Instead, it is
+            printed to stderr and the C-level callback is made to return a
+            default value.
         """
         @functools.wraps(callback)
-        def wrapper(self, modsec):
-            callback()
-            return
+        def wrapper(data, message):
+            if data == _NULL:
+                data = None
+            else:
+                data = _ffi.from_handle(data)
+
+            try:
+                return callback(data, utils.text(message))
+            except:
+                pass
 
         self._log_callback = _ffi.callback("void (*)(void *, const char *)",
                                            wrapper)
@@ -56,10 +71,6 @@ class ModSecurity:
         to provide support. Making it available internally and to the
         connector as well.
 
-        This information maybe will be used by a log parser. If you want to
-        update it, make it in a fashion that won't break the existent parsers.
-        (e.g. adding extra information _only_ to the end of the string)
-
         :return: ModSecurity version and platform.
         """
         retvalue = _lib.msc_who_am_i(self._modsecurity_struct)
@@ -73,10 +84,9 @@ class ModSecurity:
         which ``connector`` is consuming the API.
 
         It is strongly recommended to set a information in the following
-        pattern :
-            ConnectorName vX.Y.Z-tag (something else)
+        pattern : *ConnectorName vX.Y.Z-tag (something else)*
 
         :param connector: information about the connector as :class:`str`
         """
         return _lib.msc_set_connector_info(self._modsecurity_struct,
-                                           connector.encode())
+                                           utils.as_bytes(connector))
